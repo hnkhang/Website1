cc2396c9b12983453c45de6b1659f9fca8e3d45c
khang <khanghngcc200239@fpt.edu.vn>
2021-10-19 16:57:38 +0700

    new

--- Content.php
+++ Content.php
@@ -10,9 +10,9 @@ include_once("connection.php");
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <!--        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">-->
         
 
