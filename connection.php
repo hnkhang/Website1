f29e74c2a748a2d84ce9657fd34b35810897874f
khang <khanghngcc200239@fpt.edu.vn>
2021-10-19 16:42:26 +0700

    new

--- connection.php
+++ connection.php
@@ -1,7 +1,8 @@
 <?php
     //$conn = mysqli_connect('localhost', 'root', '', 'online_shopping') 
     //or die ("Cannot connect database".mysqli_connect_error());
-    $conn = pg_connect("postgres://zafkicklgluldy:473f6d11115453ef24454fcb86ed3665c539b77e94cf535fedc45f35801d5331@ec2-3-213-41-172.compute-1.amazonaws.com:5432/ddkj47cu0n3l0b")
+    $conn = pg_connect("postgres://zafkicklgluldy:473f6d11115453ef24454fcb86ed3665c539b77e94cf535fedc45f35801d5331@ec2-3-213-41-172.compute-1.amazonaws.com:5432/ddkj47cu0n3l0b");
+    echo "connected success";
     if(!$conn){
         die("cannot connect");
     }
