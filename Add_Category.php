8b8fd05d62923e9cd0b5be8bc129b766e487e2d8
khang <khanghngcc200239@fpt.edu.vn>
2021-11-09 15:02:52 +0700

    new

--- Add_Category.php
+++ Add_Category.php
@@ -20,9 +20,9 @@
 				echo $err;
 			}
 			else{
-				$id = htmlspecialchars(pg_real_escape_string($conn,$id));
-				$name = htmlspecialchars(pg_real_escape_string($conn,$name));
-				$des = htmlspecialchars(pg_real_escape_string($conn,$des));
+				$id=htmlspecialchars(pg_escape_string($conn,$id));
+				$name=htmlspecialchars(pg_escape_string($conn,$name));
+				$des=htmlspecialchars(pg_escape_string($conn,$des));
 				$sq = "SELECT * FROM category where cat_id ='$id' or cat_name = '$name'";
 				$result = pg_query($conn, $sq);
 				if(pg_num_rows($result)==0){
