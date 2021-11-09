477446667c6d80df81e3dde5256c6e235b3ceeb2
khang <khanghngcc200239@fpt.edu.vn>
2021-11-09 15:26:48 +0700

    new

---
+++ CusFeedBack.php
@@ -0,0 +1,52 @@
+<link rel="stylesheet" type="text/css" href="style.css"/>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <script language="javascript">
+    
+    </script>
+        <form name="frm" method="post" action="">
+        <h1>Customer feedback</h1>
+        <p>
+        
+        </p>
+        <table id="tablecategory" class="table table-striped table-bordered" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th><strong>No.</strong></th>
+                    <th><strong>Customer username</strong></th>
+                    <th><strong>Customer email</strong></th>
+                     <th><strong>Description</strong></th>
+                </tr>
+             </thead>
+
+			<tbody>
+            <?php
+                include_once("connection.php");
+                $NO = 1;
+                $result = pg_query($conn,"SELECT * FROM feedback");
+                while($row=pg_fetch_array($result, Null, PGSQL_ASSOC))
+                {
+            ?>
+			<tr>
+              <td class="cotCheckBox"><?php echo $NO;?></td>
+              <td><?php echo $row["username"];?></td>
+              <td><?php echo $row["email"];?></td>
+              <td><?php echo $row["description"];?></td>
+              
+            </tr>
+            <?php
+            $NO++;
+            }
+            ?>
+           
+			</tbody>
+        </table>  
+        
+        
+        <!--Nút Thêm mới , xóa tất cả-->
+        <div class="row" style="background-color:#FFF"><!--Nút chức nang-->
+            <div class="col-md-12">
+            	
+            </div>
+        </div><!--Nút chức nang-->
+ </form>
